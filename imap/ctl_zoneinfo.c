cyrusimap/cyrus-imapd

configure.in, ctl_zoneinfo.c: add xmlFirstElementChild() and xmlNextElementSibling() for older libxml2

imap/ctl_zoneinfo.c
@@ -80,6 +80,20 @@ void do_zonedir(const char *prefix, struct hash_table *tzentries,
 		struct zoneinfo *info);
 void shut_down(int code);
 
+#ifdef HAVE_XML_FIRSTCHILD
+xmlNodePtr xmlGetNextNode(xmlNodePtr node)
+{
+    for (; node && node->type != XML_ELEMENT_NODE; node = node->next);
+    return node;
+}
+
+#define xmlFirstElementChild(parent) \
+    xmlGetNextNode(parent->children)
+
+#define xmlNextElementSibling(node) \
+    xmlGetNextNode(node->next)
+#endif
+
 
 int main(int argc, char **argv)
 {

elliefm/cyrus-imapd-old

imap/ctl_zoneinfo.c
@@ -80,6 +80,20 @@ void do_zonedir(const char *prefix, struct hash_table *tzentries,
 		struct zoneinfo *info);
 void shut_down(int code);
 
+#ifndef HAVE_XML_FIRSTCHILD
+xmlNodePtr xmlGetNextNode(xmlNodePtr node)
+{
+    for (; node && node->type != XML_ELEMENT_NODE; node = node->next);
+    return node;
+}
+
+#define xmlFirstElementChild(parent) \
+    xmlGetNextNode(parent->children)
+
+#define xmlNextElementSibling(node) \
+    xmlGetNextNode(node->next)
+#endif
+
 
 int main(int argc, char **argv)
 {